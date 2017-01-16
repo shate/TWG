11907855a1d66777f7cfd6532d8b6d83b93c06ec
shate <shate@ya.ru>
2017-01-16 19:08:14 +0300

    карусель

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	
+    $('.slider').slick({
+        slidesToShow:4
+    });
+    $('.header_nav li a, .footer_nav li a').click( function(){
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); //
+        }
+        return false;
+    });
+
+    $("a.fancy").fancybox({
+        'transitionIn' : 'none',
+        'transitionOut' : 'none',
+        'titlePosition' : 'over'
+    });
 })
